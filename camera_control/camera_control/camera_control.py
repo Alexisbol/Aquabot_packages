ajout conversion quaternions pour fix la rotation

camera_control/camera_control/camera_control.py
@@ -6,7 +6,26 @@ from sensor_msgs import *
 from std_msgs.msg import *
 from nav_msgs.msg import *
 from geometry_msgs.msg import *
+import numpy as np
 
+def euler_from_quaternion(quaternion):
+    x = quaternion.x
+    y = quaternion.y
+    z = quaternion.z
+    w = quaternion.w
+
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x * x + y * y)
+    roll = np.arctan2(sinr_cosp, cosr_cosp)
+
+    sinp = 2 * (w * y - z * x)
+    pitch = np.arcsin(sinp)
+
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y * y + z * z)
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
+
+    return roll, pitch, yaw
 
 
 class CameraControl(Node):
@@ -42,11 +61,12 @@ class CameraControl(Node):
             camera_turn_msg = Float64()
         
             odom_point = self.odom_pose.position
-            odom_angle = self.odom_pose.orientation.z
-            self.get_logger().info('odomangle: "%s"' % odom_angle)
+            odom_quaternion = self.odom_pose.orientation
+            [roll, pitch, yaw] = euler_from_quaternion(odom_quaternion)
+            self.get_logger().info('yaw: "%s"' % yaw)
             angle1 = math.atan((self.aim_point.x - odom_point.x)/(self.aim_point.y - odom_point.y))
             self.get_logger().info('angle1: "%s"' % angle1)
-            camera_turn_msg.data = (angle1 - odom_angle)
+            camera_turn_msg.data = (angle1 - yaw)
 
             self.get_logger().info('Publishing: "%s"' % camera_turn_msg.data)
             self.camera_turn_pub.publish(camera_turn_msg)