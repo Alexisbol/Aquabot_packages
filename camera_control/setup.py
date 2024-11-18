4e811256cf53d4a9f16fc026ea62564365d20927
Leon <leon.albert@eleves.ec-nantes.fr>
2024-11-18 10:21:48 +0100

    fix camera_control

--- camera_control/setup.py
+++ camera_control/setup.py
@@ -17,7 +17,7 @@ setup(
     maintainer_email='leon@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
+    #tests_require=['pytest'],
     entry_points={
         'console_scripts': [
         	'camera_control = camera_control.camera_control:main',
