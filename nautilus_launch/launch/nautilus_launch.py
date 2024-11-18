1f67d4c04071938ee85699a9a31bd4a6c68ded89
Leon <leon.albert@eleves.ec-nantes.fr>
2024-11-18 10:09:50 +0100

    ajout tracking au launch et modif tracking topics

--- nautilus_launch/launch/nautilus_launch.py
+++ nautilus_launch/launch/nautilus_launch.py
@@ -10,5 +10,7 @@ def generate_launch_description():
     sl.node('camera_qrcode','opencv_decoder.py')
 
     sl.include('aquabot_ekf', 'ekf_launch.py')
+
+    sl.node('py_pathfinding')
     
     return sl.launch_description()
