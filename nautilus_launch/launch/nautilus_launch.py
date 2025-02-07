74f0492372308986e559cbace965930a65f6fa3a
Alexisbol <alexis.bollack@gmail.com>
2025-02-07 17:13:39 +0100

    update launch configuration to enable RViz and correct map file path

--- nautilus_launch/launch/nautilus_launch.py
+++ nautilus_launch/launch/nautilus_launch.py
@@ -12,7 +12,7 @@ def generate_launch_description():
     #sl.node('camera_qrcode','opencv_decoder.py')
     
     # launch the EKF for the aquabot 
-    sl.declare_arg('rviz', False)
+    sl.declare_arg('rviz', True)
     sl.declare_arg('unify',True)
 
     with sl.group(if_arg = 'rviz'):
@@ -47,7 +47,7 @@ def generate_launch_description():
     sl.include('nav2_bringup', 'bringup_launch.py',
                launch_arguments={
                                  'map':[sl.find('nautilus_launch', 'map_400.yaml')],
-                                 'params_file' : [sl.find('nautilus_launch','nav2_params.yaml')]})
+                                 'params_file' : [sl.find('nautilus_launch','nav2_params_BACKUP.yaml')]})
 
     #sl.node('py_pathfinding', 'pathfinding')
 
