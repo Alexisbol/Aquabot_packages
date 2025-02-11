da221ce4631c856bc9c3e8023dac3f5ea4577996
mseverac <mathijs.svrc@gmail.com>
2025-02-11 10:04:47 +0100

    debug

--- update_map/update_map/add_turbines.py
+++ update_map/update_map/add_turbines.py
@@ -26,28 +26,30 @@ class Add_turbines(Node):
         self.updated_map_path = self.base_path.replace('update_map/lib/python3.10/site-packages/update_map', 'nautilus_launch/share/nautilus_launch/params/map_updated_400.png')
         self.get_logger().info(f"nouveau path  : {self.updated_map_path}")
 
+        #self.updated_map_path = "map_updated_400.png"
 
     def sub_cb(self,msg):
-        #self.get_logger().info("turbines pos get")
+        self.get_logger().info("turbines pos get")
         if self.map_updated == False :
             turbines = []
             for pos in msg.poses:
                 p=pos.position
                 turbines.append((int(p.x+400),int(-p.y+400)))
 
-            img = cv2.imread(self.map_path)  
+
+            img = cv2.imread(self.map_path) 
             if img is None:
                 self.get_logger().error("Erreur : Impossible d'ouvrir l'image.")
 
             for p in turbines:
-                #self.get_logger().info(f"Type de p : {type(p)}, Valeur : {p}")
+                self.get_logger().info(f"Type de p : {type(p)}, Valeur : {p}")
 
                 cv2.circle(img,p,20,(150,150,150),-1)
                 cv2.circle(img,p,15,(75,75,75),-1)
                 cv2.circle(img,p,10,(0,0,0),-1)
 
-            #if cv2.imwrite(self.updated_map_path, img):
-                #self.get_logger().info(f"Image mise à jour enregistrée dans : {self.updated_map_path}")
+            if cv2.imwrite(self.updated_map_path, img):
+                self.get_logger().info(f"Image mise à jour enregistrée dans : {self.updated_map_path}")
 
             if not cv2.imwrite(self.updated_map_path, img):
                 self.get_logger().error("Erreur lors de la sauvegarde de l'image.")
